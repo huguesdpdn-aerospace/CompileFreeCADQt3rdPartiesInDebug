be2b27235a05b61ea33729145d1d88f0806490c9
huguesdpdn-aerospace <huguesdpdn.aerospace@gmail.com>
2024-08-12 22:05:06 +0200

    Add QT git clone submodules

--- CompileFreeCADQt3rdPartiesInDebug.sh
+++ CompileFreeCADQt3rdPartiesInDebug.sh
@@ -7,11 +7,11 @@ then
 else
     CACHE_PATH="${HOME}/.FreeCADDebug"
 fi
-UNKNOWN_ARGUMENT=0
 INSTALL_PATH="${HOME}/FreeCADDebug"
 QT_VERSION="last"
 QT_FORCE_RECOMPILE=0
 QT_PATH=""
+QT_CURRENT_TAG=""
 PS_VERSION="last"
 PS_FORCE_RECOMPILE=0
 CN_VERSION="last"
@@ -22,7 +22,8 @@ PLATFORM_NAME=""
 PLATFORM_VERSION=""
 PACKAGES_LIST=()
 PACKAGES_INSTALL_PER=8
-PACKAGE_MANAGER_COMMAND=""
+PACKAGE_MANAGER_COMMAND_UPDATE=""
+PACKAGE_MANAGER_COMMAND_INSTALL=""
 
 parseVersionOn3Digits()
 {
@@ -42,11 +43,10 @@ parseVersionOn3Digits()
 
 parseSingleArgument()
 {
-    UNKNOWN_ARGUMENT=0
     argument="$(echo "${1}" | tr '[:upper:]' '[:lower:]' | sed 's/^--//g' | sed 's/^-//g')"
     if [ -z "${argument}" ]
     then
-	UNKNOWN_ARGUMENT=0
+	argument="${argument}"
     elif [[ "${argument}" =~ ^install-path=.* ]]
     then
 	TEMP_PATH_TO_TEST="$(echo "${argument}" | cut -c 14-)"
@@ -150,8 +150,9 @@ parseSingleArgument()
     then
 	FC_FORCE_RECOMPILE=1
     else
-	echo "[ERROR]: Unknown argument '${1}'"
-	UNKNOWN_ARGUMENT=1
+	echo "[ERROR]: Unknown argument '${1}' - Please check official documentation"
+	kill -s TERM $$
+
     fi
 }
 
@@ -163,49 +164,44 @@ checkInstallPath()
 	INSTALL_PATH=""
 	echo "[ERROR]: You are forbidden to use a relative install path - Please provide the full path"
     fi
+    if ! mkdir -p "${INSTALL_PATH}"
+    then
+	echo "[ERROR]: Fail creating the install directory in ${INSTALL_PATH} - Stopping"
+	exit 1
+    fi
 }
 
 checkArguments()
 {
-    UNKNOWN_ARGUMENT=0
     if [ ! -d "${INSTALL_PATH}" ]
     then
 	echo "[ERROR]: Invalid install path argument - See above errors"
-	UNKNOWN_ARGUMENT=1
     elif [[ -z "${QT_VERSION}" ]]
     then
 	echo "[ERROR]: Invalid QT version - See above errors"
-	UNKNOWN_ARGUMENT=1
     elif [[ -z "${PS_VERSION}" ]]
     then
 	echo "[ERROR]: Invalid PySide/Shiboken version - See above errors"
-	UNKNOWN_ARGUMENT=1
     elif [[ -z "${CN_VERSION}" ]]
     then
 	echo "[ERROR]: Invalid Coin3D version - See above errors"
-	UNKNOWN_ARGUMENT=1
     elif [[ -z "${FC_VERSION}" ]]
     then
 	echo "[ERROR]: Invalid FreeCAD version - See above errors"
-	UNKNOWN_ARGUMENT=1
     elif [[ -z "${QT_FORCE_RECOMPILE}" || ("${QT_FORCE_RECOMPILE}" != "0" && "${QT_FORCE_RECOMPILE}" != "1") ]]
     then
 	echo "[ERROR]: Invalid QT recompile argument - See above errors"
-	UNKNOWN_ARGUMENT=1
     elif [[ -z "${PS_FORCE_RECOMPILE}" || ("${PS_FORCE_RECOMPILE}" != "0" && "${PS_FORCE_RECOMPILE}" != "1") ]]
     then
 	echo "[ERROR]: Invalid PySide/Shiboken recompile argument - See above errors"
-	UNKNOWN_ARGUMENT=1
     elif [[ -z "${CN_FORCE_RECOMPILE}" || ("${CN_FORCE_RECOMPILE}" != "0" && "${CN_FORCE_RECOMPILE}" != "1") ]]
     then
 	echo "[ERROR]: Invalid Coind3D recompile argument - See above errors"
-	UNKNOWN_ARGUMENT=1
     elif [[ -z "${FC_FORCE_RECOMPILE}" || ("${FC_FORCE_RECOMPILE}" != "0" && "${FC_FORCE_RECOMPILE}" != "1") ]]
     then
 	echo "[ERROR]: Invalid FreeCAD recompile argument - See above errors"
-	UNKNOWN_ARGUMENT=1
     else
-	UNKNOWN_ARGUMENT=0
+	mkdir -p "${CACHE_PATH}"	
     fi
 }
 
@@ -241,15 +237,18 @@ setPackagesToInstall()
 {
     if [[ "${PLATFORM_NAME}" == "ubuntu" && "${PLATFORM_VERSION}" =~ 24.* ]]
     then
-	PACKAGE_MANAGER_COMMAND="apt-get install -y"
+	PACKAGE_MANAGER_COMMAND_UPDATE="apt-get update"
+	PACKAGE_MANAGER_COMMAND_INSTALL="apt-get install -y"
 	PACKAGES_LIST=( "build-essential" "cmake" "valgrind" "python3" "ninja-build" "git" "perl")
     else
-	PACKAGE_MANAGER_COMMAND=""
+	PACKAGE_MANAGER_COMMAND_UPDATE=""
+	PACKAGE_MANAGER_COMMAND_INSTALL=""
 	PACKAGES_LIST=()
 	echo "[ERROR]: Your platform is not managed by this script - Your platform: [${PLATFORM_NAME}] and version [${PLATFORM_VERSION}]"
 	echo "[ERROR]: If you wish, you can request its implementation by:"
 	echo "[ERROR]: creating an issue on GIT: https://github.com/huguesdpdn-aerospace/CompileFreeCADQt3rdPartiesInDebug/issues"
 	echo "[ERROR]:     on the FreeCAD forum: https://forum.freecad.org/viewforum.php?f=4&sid=492b04c6ea9185bc2a97f3115ce31dac by tagging @huguesdpdn-aerospace"
+	kill -s TERM $$
     fi
 }
 
@@ -259,26 +258,29 @@ installPackages()
     then
 	find "${CACHE_PATH}" -type f -name INSTALL_OK -mtime +60 -delete
     fi
-    if [ ! -f "${CACHE_PATH}/INSTALL_OK" -a -n "${PACKAGE_MANAGER_COMMAND}" ]
+    if [ ! -f "${CACHE_PATH}/INSTALL_OK" -a -n "${PACKAGE_MANAGER_COMMAND_INSTALL}" ]
     then
+	sudo ${PACKAGE_MANAGER_COMMAND_UPDATE}
 	packages_list_to_install=()
 	for package_to_install in ${PACKAGES_LIST[@]}
 	do
 	    packages_list_to_install+=("${package_to_install}")
 	    if [[ ${#packages_list_to_install[@]} -ge ${PACKAGES_INSTALL_PER} ]]
 	    then
-		if ! sudo ${PACKAGE_MANAGER_COMMAND} ${packages_list_to_install[@]}
+		if ! sudo ${PACKAGE_MANAGER_COMMAND_INSTALL} ${packages_list_to_install[@]}
 		then
-		    echo "[ERROR]: Did not succeed to install packages ${packages_list_to_install[@]}"
+		    echo "[ERROR]: Did not succeed to install packages ${packages_list_to_install[@]} - Check above errors"
+		    kill -s TERM $$
 		fi
 		packages_list_to_install=()
 	    fi
 	done
 	if [[ ${#packages_list_to_install[@]} -gt 0 ]]
 	then
-	    if ! sudo ${PACKAGE_MANAGER_COMMAND} ${packages_list_to_install[@]}
+	    if ! sudo ${PACKAGE_MANAGER_COMMAND_INSTALL} ${packages_list_to_install[@]}
 	    then
-		echo "[ERROR]: Did not succeed to install packages ${packages_list_to_install[@]}"
+		echo "[ERROR]: Did not succeed to install packages ${packages_list_to_install[@]} - Check above errors"
+		kill -s TERM $$
 	    fi
 	    packages_list_to_install=()
 	fi
@@ -289,7 +291,7 @@ installPackages()
 downloadQT()
 {
     cd "${INSTALL_PATH}"
-    QT_PATH="${INSTALL_PATH}/${QT_VERSION}"
+    QT_PATH="${INSTALL_PATH}/QT_${QT_VERSION}"
     if [ ! -d "${QT_PATH}" ]
     then
 	mkdir -p "${QT_PATH}"
@@ -303,50 +305,87 @@ downloadQT()
 	    git clone https://code.qt.io/qt/qt5.git "${QT_PATH}"
 	    cd "${QT_PATH}"
 	fi
-	
+	if ! git describe --exact-match --tags > /dev/null 2>&1
+	then
+	    QT_CURRENT_TAG=""
+	else
+	    QT_CURRENT_TAG="$(git describe --exact-match --tags)"
+	fi
+	if [[ "${QT_CURRENT_TAG}" != "v${QT_VERSION}" ]]
+	then
+	    TAGS_LIST=( $(git --no-pager tag --list | grep --color=no -E '^v[0-9]+\.[0-9]+\.[0-9]+$' | tr '\n' ' ') )
+	    TAG_VERSION=""
+	    for git_tag in ${TAGS_LIST[@]}
+	    do
+		if [[ "${git_tag}" == "v${QT_VERSION}" ]]
+		then
+		    TAG_VERSION="${git_tag}"
+		fi
+	    done
+	    if [[ -z "${TAG_VERSION}" ]]
+	    then
+		echo "[ERROR]: Tag version '${QT_VERSION}' not found in QT repository - Cleaning... and aborting"
+		cd "${INSTALL_PATH}"
+		rm -rf "${QT_PATH}"
+		kill -s TERM $$
+	    fi
+	    if git checkout "${TAG_VERSION}"
+	    then
+		QT_CURRENT_TAG="${TAG_VERSION}"
+	    else
+		echo "[ERROR]: Fail switching on tag '${TAG_VERSION}' - Cleaning... and aborting"
+		cd "${INSTALL_PATH}"
+		rm -rf "${QT_PATH}"
+		kill -s TERM $$
+	    fi
+	fi
+	if [ -f "init-repository" ]
+	then
+	    chmod +x "init-repository"
+	    if ! perl init-repository -f
+	    then
+		echo "[ERROR]: Failure while downloading all QT submodules ... will retry in 5 seconds - Check your internet connection"
+		sleep 5
+		if ! perl init-repository -f
+		then
+		    echo "[ERROR]: Failure while downloading all QT submodules ... will retry in 60 seconds - Check your internet connection"
+		    sleep 60
+		    if ! perl init-repository -f
+		    then
+			echo "[ERROR]: Failure while downloading all QT submodules ... will retry in 5 minutes - Check your internet connection"
+			sleep 300
+			if ! perl init-repository -f
+			then
+			    echo "[ERROR]: Failure while downloading all QT submodules ... will retry in 1 hour - Check your internet connection"
+			    sleep 3600
+			    if ! perl init-repository -f
+			    then
+				echo "[ERROR]: Failure while downloading all QT submodules ... too many retries - Check your internet connection"
+				echo "[ERROR]: Do not worry, what have alredy been downloaded will be kept"
+				echo "[ERROR]: Just launch this script again and it will resume download where it has stopped" 
+				kill -s TERM $$
+			    fi
+			fi
+		    fi
+		fi
+	    fi
+	fi
     fi
 }
 
-total_unknown_arguments=0
 parseSingleArgument "${1}"
-((total_unknown_arguments=total_unknown_arguments+UNKNOWN_ARGUMENT))
 parseSingleArgument "${2}"
-((total_unknown_arguments=total_unknown_arguments+UNKNOWN_ARGUMENT))
 parseSingleArgument "${3}"
-((total_unknown_arguments=total_unknown_arguments+UNKNOWN_ARGUMENT))
 parseSingleArgument "${4}"
-((total_unknown_arguments=total_unknown_arguments+UNKNOWN_ARGUMENT))
 parseSingleArgument "${5}"
-((total_unknown_arguments=total_unknown_arguments+UNKNOWN_ARGUMENT))
 parseSingleArgument "${6}"
-((total_unknown_arguments=total_unknown_arguments+UNKNOWN_ARGUMENT))
 parseSingleArgument "${7}"
-((total_unknown_arguments=total_unknown_arguments+UNKNOWN_ARGUMENT))
 parseSingleArgument "${8}"
-((total_unknown_arguments=total_unknown_arguments+UNKNOWN_ARGUMENT))
 parseSingleArgument "${9}"
-((total_unknown_arguments=total_unknown_arguments+UNKNOWN_ARGUMENT))
-if [[ $total_unknown_arguments -gt 0 ]]
-then
-    echo "[ERROR]: Unknown arguments detected, stopping"
-    exit 1
-fi
-if ! mkdir -p "${INSTALL_PATH}"
-then
-    echo "[ERROR]: Fail creating the install directory in ${INSTALL_PATH} - Stopping"
-    exit 1
-fi
-
-mkdir -p "${CACHE_PATH}"
-
 gitPullMyOwnRepo
 determinePlatform
 checkInstallPath
 checkArguments
 setPackagesToInstall
-if [[ -z "${PACKAGE_MANAGER_COMMAND}" ]]
-then
-    exit 1
-fi
 installPackages
 downloadQT
